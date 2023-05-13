5b7b2aaed1cd3d582c5d04f50751208189900cac
Saïd Aarab <sayid.aarab@gmail.com>
2023-05-14 01:20:39 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+import django_heroku
+django_heroku.settings(locals())
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
