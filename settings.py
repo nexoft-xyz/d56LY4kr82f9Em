e949047a870792cb602afdfb8810f3ec9d7ee47d
Saïd Aarab <sayid.aarab@gmail.com>
2023-05-14 01:21:58 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
-import django_heroku
+import django_heroku dj_database_url
 django_heroku.settings(locals())
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
